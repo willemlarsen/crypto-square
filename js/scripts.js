c81d0e92c5eb31eea37c30367b31bbd96f4fe5a6
Willem Larsen <willemlarsen@gmail.com>
2014-06-11 08:43:34 -0700

    include regex search and pass second test

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ var prepareCryptoTextA = function (text) {
 	return text.toLowerCase();
 };
 
+var prepareCryptoTextB = function (text) {
+	text = text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	text = text.replace(/\s{2, 100}/g, " ");
+	return text
+}
+
 
--- spec/specs.js
+++ spec/specs.js
@@ -2,5 +2,8 @@ describe('cryptoSquare', function() {
 	it('takes a block of text, and downcases it', function() {
 		prepareCryptoTextA('Have a happy day!').should.equal("have a happy day!");
 	});
+	it('takes a block of downcased text, and removes punctuation', function() {
+		prepareCryptoTextB("have a happy day!").should.equal("have a happy day");
+	});
 });
 
