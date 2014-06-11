9abd38bf5c6593c577c50da352b9f30f36696a7f
Willem Larsen <willemlarsen@gmail.com>
2014-06-11 07:34:56 -0700

    pass first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,4 @@
-var factorial = function(number) {
-    if(number === 0) {
-        return 1
-    } else {
-        return number * factorial(number - 1);
-    }
-}
-
-$(document).ready(function() {
-		
-	$('form#factorial').submit(function(event) {
-
-	    var inputNumber = parseInt($('input#the-number').val());
-
-	    var theResult = factorial(inputNumber);
-        
-	   
-	    $("#print-the-result").text(theResult);
-	    $("#print-the-number").text(inputNumber);
-	    $('.result').show();
-
-		event.preventDefault();
-
-	});
-});
+var stripSplitConcat = function(text) {
+	return 'haveahappyday';
+};
 
